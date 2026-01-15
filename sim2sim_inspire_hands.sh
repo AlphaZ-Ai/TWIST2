56bf149240a10337ee26245089fbae41528c4bbb
Mcen25 <matthew.en.c@gmail.com>
2026-01-15 15:23:44 -0800

    Dex-retargeting git added as a submodule

--- sim2sim_inspire_hands.sh
+++ sim2sim_inspire_hands.sh
@@ -4,12 +4,13 @@
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 SCRIPT_DIR=$(dirname $(realpath $0))
+#Needs a new checkpoint for the inspire hands
 ckpt_path=${SCRIPT_DIR}/assets/ckpts/twist2_1017_20k.onnx
 
 cd deploy_real
 
 python server_low_level_g1_sim.py \
-    --xml ../assets/g1/g1_sim2sim_29dof.xml \
+    --xml ../assets/g1/inspire_robot/g1_29dof_with_inspire_rev_1_0.xml \
     --policy ${ckpt_path} \
     --device cuda \
     --measure_fps 1 \
