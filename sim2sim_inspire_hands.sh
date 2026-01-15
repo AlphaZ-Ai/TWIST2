8a0fdf2aa9f62f9b09d2b1a6ffb10c516e1a38f4
Mcen25 <matthew.en.c@gmail.com>
2026-01-15 15:11:48 -0800

    Inspire assets init with URDF/XML

---
+++ sim2sim_inspire_hands.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set LD_LIBRARY_PATH to include conda environment lib directory
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+ckpt_path=${SCRIPT_DIR}/assets/ckpts/twist2_1017_20k.onnx
+
+cd deploy_real
+
+python server_low_level_g1_sim.py \
+    --xml ../assets/g1/g1_sim2sim_29dof.xml \
+    --policy ${ckpt_path} \
+    --device cuda \
+    --measure_fps 1 \
+    --policy_frequency 100 \
+    --limit_fps 1 \
+    # --record_proprio \
